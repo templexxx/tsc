af2cde79276acd9c8792e83738abb433b7ab3553
templexxx <lucas1x1x@gmail.com>
2020-10-15 15:33:58 +0800

    delete useless functions and files

--- freq.go
+++
@@ -1,8 +0,0 @@
-package tsc
-
-func GetFrequency() int64 {
-	if !Enabled {
-		return 0
-	}
-	return 0
-}
--- tsc_amd64.go
+++ tsc_amd64.go
@@ -89,16 +89,6 @@ func enableTSC() bool {
 	return true
 }
 
-// calibrateTime is the min time to calibrate clocks.
-var calibrateTime = 20 * time.Microsecond
-
-// SetCalibrateTime sets a new min time to calibrate clocks.
-// Warn:
-// Not thread-safe。
-func SetCalibrateTime(t time.Duration) {
-	calibrateTime = t
-}
-
 // Calibrate calibrates tsc & wall clock.
 //
 // It's a good practice that run Calibrate periodically (every hour) outside,
@@ -112,57 +102,6 @@ func Calibrate() {
 	}
 
 	calibrateOnce()
-}
-
-// getMinDeltaToWall gets the smallest gap between wall clock and tsc.
-func getMinDeltaToWall() (tsc, wall uint64) {
-	// 1024 is enough for finding lowest wall clock cost in most cases.
-	// Although time.Now() is using VDSO to get time, but it's unstable,
-	// sometimes it will take more than 1000ns,
-	// we have to use a big loop(e.g. 1024) to get the "real" clock.
-	n := 1024
-	timeline := make([]uint64, n+n+1)
-
-	timeline[0] = getInOrder()
-	// [tsc, wc, tsc, wc, ..., tsc]
-	for i := 1; i < len(timeline)-1; i += 2 {
-		timeline[i] = uint64(time.Now().UnixNano())
-		timeline[i+1] = getInOrder()
-	}
-
-	// The minDelta is the smallest gap between two adjacent tscs,
-	// which means the smallest gap between wall clock and tsc too.
-	minDelta := uint64(math.MaxUint64)
-	minIndex := 1 // minIndex is wall clock index where has minDelta.
-
-	// time.Now()'s precision is only µs (on MacOS),
-	// which means we will get multi same wall clock in timeline,
-	// and the middle one is closer to the real time in statistics.
-	// Try to find the minimum delta when wall clock is in the "middle".
-	for i := 1; i < len(timeline)-1; i += 2 {
-		last := timeline[i]
-		for j := i + 2; j < len(timeline)-1; j += 2 {
-			if timeline[j] != last {
-				mid := (i + j - 2) >> 1
-				if isEven(mid) {
-					mid++
-				}
-
-				delta := timeline[mid+1] - timeline[mid-1]
-				if delta < minDelta {
-					minDelta = delta
-					minIndex = mid
-				}
-
-				i = j
-				last = timeline[j]
-			}
-		}
-	}
-
-	tsc = (timeline[minIndex+1] + timeline[minIndex-1]) >> 1
-	wall = timeline[minIndex]
-	return
 }
 
 // fastCalibrate calibrates tsc clock and wall clock fastly,
